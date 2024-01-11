3398c2023e0d89e7e4f9ea40b51fe26a1c7e6242
Marcusviniciuscardoso <m.v.cardoso.r@gmail.com>
2024-01-11 16:39:40 -0300

    fix: database-column-type

--- database/blog.sql
+++ database/blog.sql
@@ -2,7 +2,7 @@ CREATE TABLE profile (
   idprofile SERIAL PRIMARY KEY,
   name VARCHAR(30) NOT NULL,
   username VARCHAR(50) NOT NULL,
-  dateofbirth VARCHAR,
+  dateofbirth VARCHAR(45) NOT NULL,
   avatar VARCHAR(255), 
   credentialid INT
 );
@@ -11,42 +11,40 @@ CREATE TABLE credentials (
   idcredentials SERIAL PRIMARY KEY,
   username VARCHAR(255) NOT NULL,
   userpassword VARCHAR(45) NOT NULL,
-  profileid INT,
-  FOREIGN KEY (profileid) REFERENCES profile(idprofile)
 );
 
 CREATE TABLE cards (
   idcard SERIAL PRIMARY KEY,
-  title VARCHAR(30),
-  subtitle VARCHAR(50),
-  creationdate VARCHAR,
-  featuredimage TEXT,
-  profileid INT,
+  title VARCHAR(30) NOT NULL,
+  subtitle VARCHAR(50) NOT NULL,
+  creationdate VARCHAR(45) NOT NULL,
+  featuredimage TEXT NOT NULL,
+  profileid INT NOT NULL,
   FOREIGN KEY (profileid) REFERENCES profile(idprofile)
 );
 
 CREATE TABLE cardtexts (
   idcardtexts SERIAL PRIMARY KEY,
-  text TEXT,
-  cardidtexts INT,
+  text TEXT NOT NULL,
+  cardidtexts INT NOT NULL,
   FOREIGN KEY (cardidtexts) REFERENCES cards(idcard)
 );
 
 CREATE TABLE cardimages (
   idcardimages SERIAL PRIMARY KEY,
-  images VARCHAR(255), 
-  cardidimages INT,
+  images VARCHAR(255) NOT NULL, 
+  cardidimages INT NOT NULL,
   FOREIGN KEY (cardidimages) REFERENCES cards(idcard)
 );
 
 CREATE TABLE commentssection (
   idcomment SERIAL PRIMARY KEY,
-  comment TEXT,
-  likes INT,
-  dislikes INT,
-  profileid INT,
-  commentdate INT,
-  cardid INT,
+  comment TEXT NOT NULL,
+  likes INT NOT NULL,
+  dislikes INT NOT NULL,
+  commentdate varchar(45) NOT NULL,
+  profileid INT NOT NULL,
+  cardid INT NOT NULL,
   FOREIGN KEY (profileid) REFERENCES profile(idprofile),
   FOREIGN KEY (cardid) REFERENCES cards(idcard)
 );
